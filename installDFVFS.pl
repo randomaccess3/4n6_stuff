e582a95e4dfacead32bbb54244e4b97037f7eab7
randomaccess3 <randomaccess3@gmail.com>
2016-06-28 16:12:30 +1000

    updated unzip code to work with unicode filename
    Added code for unicode support; appears to work

--- installDFVFS.pl
+++ installDFVFS.pl
@@ -5,15 +5,14 @@
 # Blog: thisweekin4n6.wordpress.com
 
 # InstallDFVFS
-# version 0.03
+# version 0.04
 
 # 0.01 - Initial commit
 # 0.02 - Fix error in downloading x64 packages
 # 0.03 - add comment for requiring version 1.57, adjusted python call to avoid path issues
+# 0.04 - 1.57 of Archive::Zip isn't required, added additional code for dealing with unicode taken from perlmonks. This script needs Win32::Unicode, which occasionally fails the tests on install. Install using cpan -f Win32::Unicode.
 
 # This assumes that Python2.7 is installed
-
-# Theres an error when unzipping the files from master.zip, the tests will pass if you extract the one file it fails on using something like winrar
 
 # TO DO:
 # Check for crypto mispelling - I think this may have been fixed in the newly released version
@@ -21,12 +20,174 @@
 # SQLite Version/URL is hard linked and could be updated to automatically detect location of latest version
 # Assumes no libraries have been installed - add section to detect if libraries installed are up to date or need to be uninstalled before the newer version is installed
 # Add backports lzma download - tests are skipped (https://github.com/peterjc/backports.lzma)
+# Cleanup subroutine redefinition
+
+use utf8;
+use Win32::Unicode();
 
 use warnings;
 use strict;
 use WWW::Mechanize;
 use version;
-use Archive::Zip qw( :ERROR_CODES ); #requires version 1.57
+use Archive::Zip qw( :ERROR_CODES );
+
+# ======================================================================================================
+# Unicode Unzip Code taken from perlmonks
+# http://perlmonks.org/?node_id=1113291
+sub Archive::Zip::Archive::extractTree {
+    package Archive::Zip::Archive;
+    my $self = shift;
+
+    my ( $root, $dest, $volume );
+    if ( ref( $_[0] ) eq 'HASH' ) {
+        $root   = $_[0]->{root};
+        $dest   = $_[0]->{zipName};
+        $volume = $_[0]->{volume};
+    }
+    else {
+        ( $root, $dest, $volume ) = @_;
+    }
+
+    $root = '' unless defined($root);
+    $dest = './' unless defined($dest);
+    my $pattern = "^\Q$root";
+    my @members = $self->membersMatching($pattern);
+
+    foreach my $member (@members) {
+        my $fileName = $member->fileName();           # in Unix format
+        $fileName =~ s{$pattern}{$dest};    # in Unix format
+                                            # convert to platform format:
+        $fileName = Archive::Zip::_asLocalName( $fileName, $volume );
+#~         ::dd( 'fileName' => $fileName );
+        my $status = $member->extractToFileNamed($fileName);
+        return $status if $status != AZ_OK;
+    }
+    return AZ_OK;
+}
+
+sub Archive::Zip::DirectoryMember::extractToFileNamed {
+    package Archive::Zip::DirectoryMember;
+    my $self    = shift;
+    my $name    = shift;                                 # local FS name
+    my $attribs = $self->unixFileAttributes() & 07777;
+#~     ::dd( 'name' => $name );
+#~     mkpath( $name, 0, $attribs );                        # croaks on error
+#~     utime( $self->lastModTime(), $self->lastModTime(), $name );
+    use Encode();
+    $name = Encode::decode('UTF-8', $name );
+    Win32::Unicode::mkpathW( $name ) or die "Cannot mkpathW( $name ): $!";
+    Win32::Unicode::utimeW( $self->lastModTime(), $self->lastModTime(), $name );
+    return AZ_OK;
+}
+
+
+sub Archive::Zip::Member::extractToFileNamed {
+    package Archive::Zip::Member;
+    my $self = shift;
+
+    # local FS name
+    my $name = ( ref( $_[0] ) eq 'HASH' ) ? $_[0]->{name} : $_[0];
+    $self->{'isSymbolicLink'} = 0;
+
+    # Check if the file / directory is a symbolic link or not
+    if ( $self->{'externalFileAttributes'} == 0xA1FF0000 ) {
+        $self->{'isSymbolicLink'} = 1;
+        $self->{'newName'} = $name;
+#~         ::dd( 'newName' => $name );
+#~         my ( $status, $fh ) = _newFileHandle( $name, 'r' );
+#~         my $retval = $self->extractToFileHandle($fh);
+        my $fh = Win32::Unicode::File->new( '<', $name ); ## WHATEVER
+        $fh->binmode;
+        my $retval = $self->extractToFileHandle($fh);
+        $fh->close();
+    } else {
+        #return _writeSymbolicLink($self, $name) if $self->isSymbolicLink();
+        return _error("encryption unsupported") if $self->isEncrypted();
+#~         
+#~         ::dd( 'dirname' => dirname($name) );
+#~         mkpath( dirname($name) );    # croaks on error
+#~         my ( $status, $fh ) = _newFileHandle( $name, 'w' );
+#~         return _ioError("Can't open file $name for write") unless $status;
+#~         my $retval = $self->extractToFileHandle($fh);
+#~         $fh->close();
+#~         chmod ($self->unixFileAttributes(), $name)
+#~             or return _error("Can't chmod() ${name}: $!");
+#~         utime( $self->lastModTime(), $self->lastModTime(), $name );
+#~         return $retval;
+
+        use Encode();
+        $name = Encode::decode('UTF-8', $name );
+        my $dir = dirname($name);
+        Win32::Unicode::mkpathW( $dir ) or die "Cannot mkpathW( $dir): $!";
+        
+        my $fh = Win32::Unicode::File->new( '>', $name )
+            or return _ioError("Can't open file $name for write: $!");
+        $fh->binmode;
+        my $retval = $self->extractToFileHandle($fh);
+        $fh->close();
+        
+        Win32::Unicode::utimeW( $self->lastModTime(), $self->lastModTime(), $name );
+        return $retval;
+    }
+}
+# If I already exist, extraction is a no-op.
+sub Archive::Zip::NewFileMember::extractToFileNamed {
+    package Archive::Zip::NewFileMember;
+    my $self = shift;
+    my $name = shift;    # local FS name
+#~     if ( File::Spec->rel2abs($name) eq
+#~         File::Spec->rel2abs( $self->externalFileName() ) and -r $name )
+    if ( Win32::Unicode::Util::rel2abs($name) eq
+         Win32::Unicode::Util::rel2abs( $self->externalFileName() )
+         and Win32::Unicode::File->new( '<', $name )
+    )
+    {
+        return AZ_OK;
+    }
+    else {
+        return $self->SUPER::extractToFileNamed( $name, @_ );
+    }
+}
+
+
+# Return an opened IO::Handle
+# my ( $status, fh ) = _newFileHandle( 'fileName', 'w' );
+# Can take a filename, file handle, or ref to GLOB
+# Or, if given something that is a ref but not an IO::Handle,
+# passes back the same thing.
+sub Archive::Zip::_newFileHandle {
+    package Archive::Zip;
+    my $fd     = shift;
+    my $status = 1;
+    my $handle;
+
+    if ( ref($fd) ) {
+        if ( _ISA($fd, 'IO::Scalar') or _ISA($fd, 'IO::String') ) {
+            $handle = $fd;
+        } elsif ( _ISA($fd, 'IO::Handle') or ref($fd) eq 'GLOB' ) {
+            $handle = IO::File->new;
+            $status = $handle->fdopen( $fd, @_ );
+        } else {
+            $handle = $fd;
+        }
+    } else {
+#~         $handle = IO::File->new;
+#~         $status = $handle->open( $fd, @_ );
+        my( $mode ) = @_;
+        my $name = Encode::decode('UTF-8', $fd );
+        $handle = Win32::Unicode::File->new( $mode, $name ) or do { $status = $!; };
+    }
+
+    return ( $status, $handle );
+}
+#=========================================================================================
+
+
+
+
+
+
+
 
 #Unzips an archive to a single folder (doesn't work well for archives with folders)
 sub unzipArchive($$){
@@ -224,16 +385,26 @@ print "------------------------------------------------------------------------\
 print "Download and Run Tests\n";
 print "------------------------------------------------------------------------\n";
 my $testsArchive = "master.zip";
-my $testsDirectory = "master/";
+my $testsDirectory = "dfvfs-master";
 downloadFile($testsArchive, "https://github.com/log2timeline/dfvfs/archive");
 
 print "Extracting DFVFS\n";
-my $zip = Archive::Zip->new();
-die 'read error' unless ( $zip->read( $testsArchive ) == AZ_OK );
-$zip->extractTree( '', $testsDirectory );
+
+my $zip     = Archive::Zip->new();
+my $zipName = $testsArchive;
+my $zip_status  = $zip->read($zipName);
+die "Read of $zipName failed\n" if $zip_status != AZ_OK;
+
+$zip->extractTree();
+
+
+#my $zip = Archive::Zip->new();
+#die 'read error' unless ( $zip->read( $testsArchive ) == AZ_OK );
+#$zip->extractTree( '', $testsDirectory );
+
 # The tests will fail if you dont run them from the directory that run_tests.py is in
 # For future reference this is why: https://stackoverflow.com/questions/7009055/how-do-i-cd-into-a-directory-using-perl
-chdir('master\\dfvfs-master\\') or die "$!";
+chdir($testsDirectory) or die "$!";
 system('C:\python27\python.exe "run_tests.py"');
 	
 print "\n You're good to go if the tests all say OK!\n";
