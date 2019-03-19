56a5b0a841af8f5561ed579dd99980501adeef08
Phill Moore <randomaccess3@gmail.com>
2019-03-19 14:08:19 +0800

    changed .dat
    *.dat is probably a bit too inclusive

--- registryExplorerSession.ps1
+++ registryExplorerSession.ps1
@@ -1,1 +1,1 @@
-'["' + $((get-childitem -include "*.DAT", "SAM", "SYSTEM", "SOFTWARE", "SECURITY" -Recurse -Force) -replace('\\', '\\') -join '","')+'"]' > reg.re_proj
+'["' + $((get-childitem -include "NTUSER.DAT", "USRCLASS.DAT", "SAM", "SYSTEM", "SOFTWARE", "SECURITY" -Recurse -Force) -replace('\\', '\\') -join '","')+'"]' > reg.re_proj
